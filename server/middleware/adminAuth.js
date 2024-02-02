ff84b54d5c6b323c262f0db4471e509b4bc6dbf3
SimenMH <simenmelo@gmail.com>
2024-02-02 03:33:53 +0100

    console log password

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -4,6 +4,8 @@ const PASSWORD = 'abc123';
 export const adminAuth = (req, res, next) => {
   const { password } = req.body;
 
+  console.log(password);
+
   if (password !== PASSWORD) {
     res.status(401);
     throw new Error('Unauthorized');
