a64f6ac414fe78f48bad22d742cbb13e9c37063d
SimenMH <simenmelo@gmail.com>
2025-09-11 15:48:37 +0200

    fix admin auth key header name

--- server/middleware/adminAuth.js
+++ server/middleware/adminAuth.js
@@ -2,9 +2,8 @@ const adminAuthKey = process.env.ADMIN_AUTH_KEY;
 
 // Verify password
 export const adminAuth = (req, res, next) => {
-  const { AuthKey } = req.headers;
-
-  if (AuthKey !== adminAuthKey) {
+  const { authkey } = req.headers;
+  if (authkey !== adminAuthKey) {
     res.status(401);
     throw new Error('Unauthorized');
   }
